ad53974cdcc2cd295f02154154b7533d6020da38
Rosen Toshev <rtoscheff@gmail.com>
2023-02-19 22:57:31 +0000

    Fixed the ApolloClient and dropdown testing issues

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,8 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
+import fetch from 'cross-fetch';
 
 // Initialize Apollo Client
 export const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com',
+  link: new HttpLink({uri: 'https://countries.trevorblades.com', fetch}),
   cache: new InMemoryCache(),
 });
