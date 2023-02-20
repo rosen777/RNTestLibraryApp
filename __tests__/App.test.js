692e4c2aa63cd15aacba02555fbcf76be36b9e6f
Rosen Toshev <rtoscheff@gmail.com>
2023-02-20 00:30:22 +0000

    Fixed the tests using await waitFor

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import 'react-native';
 import App from '../App';
-import {render, fireEvent} from '@testing-library/react-native';
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 
 test('renders correctly', () => {
   const {toJSON} = render(<App />);
@@ -11,21 +16,25 @@ test('renders correctly', () => {
 const component = <App />;
 
 describe('Check input field data', () => {
-  test('Displays a username, if the username field has been completed', () => {
+  test('Displays a username, if the username field has been completed', async () => {
     const INPUT_TEXT = 'John';
-    const {getByTestId, getByText} = render(component);
-    const userNameTextInput = getByTestId('App.username');
-    fireEvent.changeText(userNameTextInput, INPUT_TEXT);
-    const username = getByText(INPUT_TEXT);
-    expect(username).toBeTruthy();
+    const {getByText, getByPlaceholderText} = render(component);
+    await waitFor(() => {
+      const nameInput = getByPlaceholderText('Name');
+      fireEvent.changeText(nameInput, INPUT_TEXT);
+      const username = getByText(INPUT_TEXT);
+      expect(username).toBeTruthy();
+    });
   });
 
-  test('Displays a userage, if the userage field has been completed', () => {
+  test('Displays a userage, if the userage field has been completed', async () => {
     const INPUT_TEXT = '25';
     const {getByTestId, getByText} = render(component);
-    const userAgeTextInput = getByTestId('App.userage');
-    fireEvent.changeText(userAgeTextInput, INPUT_TEXT);
-    const userage = getByText(INPUT_TEXT);
-    expect(userage).toBeTruthy();
+    await waitFor(() => {
+      const userAgeTextInput = getByTestId('App.userage');
+      fireEvent.changeText(userAgeTextInput, INPUT_TEXT);
+      const userage = getByText(INPUT_TEXT);
+      expect(userage).toBeTruthy();
+    });
   });
 });
